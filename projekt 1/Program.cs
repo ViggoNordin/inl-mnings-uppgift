b2b03e761a8f542d6f7e0c6e564258dffcab10e4
ViggoNordin <vino05001@utb.vaxjo.se>
2022-10-24 08:57:20 +0200

    färdigt med projetk

---
+++ projekt 1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+{
+ 
+
+Random rd = new Random();
+int randnum = rd.Next(1,101);
+int tal; 
+int lägsta = 1;
+int högsta = 100;
+tal=  lägsta + högsta+1;
+int antalgis= 0;
+
+ while(tal!= randnum)
+{
+    Console.WriteLine("Gissa talet mellan 1 och 100");
+    tal = Int32.Parse(Console.ReadLine());
+if(randnum<tal)
+    Console.WriteLine("Lägre");
+ 
+else if(randnum>tal)
+     Console.WriteLine("Högre");
+ 
+ antalgis++;
+}
+
+  Console.WriteLine("Du hade rätt");
+  Console.WriteLine("Antalgissningar"+antalgis);
+
+}
